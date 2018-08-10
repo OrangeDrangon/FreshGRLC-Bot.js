b42fd72be584133e9d06cfe9d7d89efbe459ea3f
Kyle Rosenberg <kyle@ekrosenberg.com>
2018-08-09 21:21:46 -0400

    Updated cmc command to work

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,17 @@
 import axios from 'axios';
 import discord, { Message, TextChannel } from 'discord.js';
 import mongoose from 'mongoose';
+import NodeCache from 'node-cache';
 
 import { BOTNAME, CMCAPIKEY, DATABASEURL, LOGOURL, POOLAPIADDRESS, TOKEN } from './config';
 import { IServerConfig, ServerConfigModel } from './models/serverConfig';
-
-let cryptoInfo: any[] | undefined;
 
 const client = new discord.Client();
 client.login(TOKEN);
 
 mongoose.connect(DATABASEURL, { useNewUrlParser: true });
+
+const cache = new NodeCache({ stdTTL: 60 * 5, checkperiod: 60 * 6 });
 
 client.on('message', async (message) => {
     if (message.author.id === client.user.id) { return; }
@@ -85,11 +86,21 @@ client.on('message', async (message) => {
         return;
     }
 
+    // Will break in december but screw the dumb api limit.. to migrate invert the comments
+    // Wait till last possible moment because of rate limitting and stupid stuff
+
     if (content[0] === 'cmc') {
-        const options = { headers: { 'X-CMC_PRO_API_KEY': CMCAPIKEY } };
+        // const options = { headers: { 'X-CMC_PRO_API_KEY': CMCAPIKEY } };
+
+        let cryptoInfo = cache.get<any[]>('cryptoInfo');
 
         if (!cryptoInfo) {
-            cryptoInfo = (await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?limit=5000', options)).data.data;
+            try {
+                // cryptoInfo = (await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?limit=5000', options)).data.data;
+                cryptoInfo = (await axios.get(`https://api.coinmarketcap.com/v2/listings/`)).data.data;
+
+                cache.set('cryptoInfo', cryptoInfo, 60 * 30);
+            } catch (error) { return await channel.send({ embed: generateEmbeded('There was an error here is some possibly technical stuff, possibly clear as day.', error.response.data) }); }
         }
 
         if (!cryptoInfo) { return; }
@@ -99,53 +110,68 @@ client.on('message', async (message) => {
         let coinInfo;
 
         for (const coin of cryptoInfo) {
-            if (content[1].toUpperCase() === coin.symbol || content[1].toLowerCase() === coin.slug) { coinInfo = coin; }
+            if (content[1].toUpperCase() === coin.symbol || content[1].toLowerCase() === coin.website_slug) { coinInfo = coin; break; }
+            // if (content[1].toUpperCase() === coin.symbol || content[1].toLowerCase() === coin.slug) { coinInfo = coin; break; }
         }
 
         if (!coinInfo) { return; }
 
-        const metadata = (await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=${coinInfo.symbol}`, options)).data.data;
+        let coinData = cache.get<{ latestInfoUSD: any, latestInfoBTC: any, logo: string }>(coinInfo.symbol);
+
+        if (!coinData) {
+            try {
+                const latestInfoUSD = (await axios.get(`https://api.coinmarketcap.com/v2/ticker/${coinInfo.id}/?convert=USD`)).data.data;
+                const latestInfoBTC = (await axios.get(`https://api.coinmarketcap.com/v2/ticker/${coinInfo.id}/?convert=BTC`)).data.data;
+                // const latestInfoUSD = (await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=${coinInfo.id}&convert=USD`, options)).data.data[coinInfo.id];
+                // const latestInfoBTC = (await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=${coinInfo.id}&convert=BTC`, options)).data.data[coinInfo.id];
+                const logo = `https://s2.coinmarketcap.com/static/img/coins/32x32/${coinInfo.id}.png`; // This will not change in december
+
+                coinData = { latestInfoUSD, latestInfoBTC, logo };
+                cache.set(coinInfo.symbol, coinData);
+            } catch (error) { return await channel.send({ embed: generateEmbeded('There was an error here is some possibly technical stuff, possibly clear as day.', error.response.data) }); }
+        }
 
         const embed = new discord.RichEmbed();
 
-        embed.setAuthor(`${coinInfo.name} Info`, metadata[coinInfo.symbol].logo, 'https://coinmarketcap.com/currencies/' + coinInfo.slug);
+        embed.setAuthor(`${coinInfo.name} Info`, coinData.logo, 'https://coinmarketcap.com/currencies/' + coinInfo.website_slug);
+        // embed.setAuthor(`${coinInfo.name} Info`, coinData.logo, 'https://coinmarketcap.com/currencies/' + coinInfo.slug);
 
         embed.setColor('DARK_GOLD');
-
-        const latestInfoUSD = (await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=${coinInfo.id}&convert=USD`, options)).data.data[coinInfo.id];
-        const latestInfoBTC = (await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=${coinInfo.id}&convert=BTC`, options)).data.data[coinInfo.id];
 
         embed.fields = [
             {
                 inline: false,
                 name: 'Rank',
-                value: latestInfoUSD.cmc_rank.toString(),
+                value: coinData.latestInfoUSD.rank.toString(),
+                // value: coinData.latestInfoUSD.cmc_rank.toString(),
             },
             {
                 inline: false,
                 name: 'Price USD',
-                value: `$${latestInfoUSD.quote.USD.price}`,
+                value: `$${coinData.latestInfoUSD.quotes.USD.price.toFixed(10)}`,
+                // value: `$${coinData.latestInfoUSD.quote.USD.price.toFixed(10)}`,
             },
             {
                 inline: false,
                 name: 'Price BTC',
-                value: `${latestInfoBTC.quote.BTC.price.toFixed(14)} BTC`,
+                value: `${coinData.latestInfoBTC.quotes.BTC.price.toFixed(10)} BTC`,
+                // value: `${coinData.latestInfoBTC.quote.BTC.price.toFixed(10)} BTC`,
             },
             {
                 inline: false,
                 name: 'Market Cap',
-                value: `$${(latestInfoUSD.quote.USD.market_cap / 100).toFixed(2)}`,
+                value: `$${(coinData.latestInfoUSD.quotes.USD.market_cap).toFixed(2)}`,
+                // value: `$${(coinData.latestInfoUSD.quote.USD.market_cap).toFixed(2)}`,
             },
             {
                 inline: false,
                 name: 'Circulating Supply',
-                value: latestInfoUSD.circulating_supply.toString(),
+                value: coinData.latestInfoUSD.circulating_supply.toString(),
             },
         ];
 
         await channel.send({ embed });
         return;
-
     }
 });
 
